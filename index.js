aecdbe9f0ce7104185c9bd0cd029778326b5e3de
Edijs Strautmanis <zyferplays48@gmail.com>
2022-05-07 15:16:07 +0300

    An update

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-const TOKEN = "OTYwOTUxOTA0Nzk4NjU4NTYw.Ykx6Mw.dCzJdmNQw7ulklxnwEeO_UroCak"
+const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -18,6 +19,16 @@ client.on("messageCreate", (message) => {
     if (message.content == "hi") {
         message.reply("Hello World!");
     }
+})
+
+const welcomeChannelId = "966047829107830804";
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN);
